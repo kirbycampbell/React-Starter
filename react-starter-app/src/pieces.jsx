f9de5325c46495ac2ee408a566e5a49e4b1a5a7e
kirbycampbell <jkirbycampbell@gmail.com>
2018-11-21 23:53:56 -0800

    Update pieces.jsx

--- react-starter-app/src/pieces.jsx
+++ react-starter-app/src/pieces.jsx
@@ -3,14 +3,23 @@ import React, { Component } from "react";
 class Pieces extends Component {
   constructor() {
     super();
-    this.name = "Mannerson";
   }
   state = {
     part: "huh",
-    things: ["thing1", "thing2", "thing3"]
+    things: ["thing1", "thing2", "thing3"],
+    name: "Mannerson"
+  };
+
+  flipName = () => {
+    let newState = { ...this.state };
+    newState.name = "Linny Pinkerson";
+    return this.setState({ newState });
   };
 
   render() {
+    setTimeout(() => {
+      this.setState({ name: "Bob" });
+    }, 1050);
     return (
       <React.Fragment>
         <div>
@@ -19,7 +28,7 @@ class Pieces extends Component {
           ))}
         </div>
         <div>{this.state.part}</div>
-        <div>{this.name}</div>
+        <button onClick={this.flipName}>{this.state.name}</button>
       </React.Fragment>
     );
   }
